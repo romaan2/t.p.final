e9c4cfd38752ea908b87120f3aa62cce7906f74d
romaan2 <57414261+romaan2@users.noreply.github.com>
2019-11-26 15:29:11 -0300

    Add files via upload

---
+++ poker.js
@@ -0,0 +1,39 @@
+
+var cabecera = document.querySelector('header');
+var section = document.querySelector('section');
+var URL = "poker2.json"
+
+
+var solicitud = new XMLHttpRequest();
+solicitud.open('GET', "poker2.json");
+solicitud.responseType = 'json';
+solicitud.send();
+
+solicitud.onload = function() {
+    var jugadores = solicitud.response;
+mostrartitulo (jugadores);
+
+}
+
+function mostrartitulo(jsonObj) {
+    var h1 = document.createElement('h1');
+    h1.textContent = "nombre"  + jsonObj["nombre"]  + "brazaletes"  + jsonObj["brazaletes"]
+    cabecera.appendChild(h1);
+}
+
+
+function mostrarinformacion (jsonObj) {
+    var infoJugadores = jsonObj ['miembros']
+
+for (var i = 0 ; i < infoJugadores.length; i++) {
+    var h2nombre = document.createElement('h2');
+    var h2apodo = document.createElement('p');
+
+h2nombre.textContent = jsonObjJugadores[i].nombre;
+h2apodo.textContent = jsonObjJugadores[i].apodo;
+
+cabecera.appendChild(h2nombre);
+cabecera.appendChild(h2apodo)
+}
+}
+
